a342398c9c27f96cc1b38de8f44e1d4fe6e99096
rollrat <rollrat.software@gmail.com>
2018-01-25 18:41:05 +0900

    Add: HashCrypto.

---
+++ rollrat-framework/rollrat-framework/rollrat-framework/Crypto/Crypto.cpp
@@ -0,0 +1,43 @@
+//===----------------------------------------------------------------------===//
+//
+//                          RollRat-FrameWork V2
+//
+//===----------------------------------------------------------------------===//
+//
+//  Crypto.h - 01/25/2018
+//
+//  Copyright (C) 2018. rollrat. All Rights Reserved.
+//
+//===----------------------------------------------------------------------===//
+
+#include "Crypto.h"
+
+using namespace ofw;
+
+void ofw::HashCrypto::Encryption(const CryptoArray& plain, CryptoArray& key)
+{
+  uint8_t *hash = Sha256::FromArray(key.Array(), key.Size());
+  uint8_t chain[256] = { 0, };
+  size_t round = plain.Size() / 8;
+  size_t remain = plain.Size() % 8;
+  for (size_t i = 0; i < round; i++)
+    ((uint64_t *)plain.Array())[i] ^= ((uint64_t *)hash)[i % 8];
+
+  uint8_t *arr = plain.Array() + round * 8;
+  for (size_t i = 0; i < remain; i++)
+    arr[i] ^= hash[i];
+}
+
+void ofw::HashCrypto::Decryption(const CryptoArray& chip, CryptoArray& key)
+{
+  uint8_t *hash = Sha256::FromArray(key.Array(), key.Size());
+  uint8_t chain[256] = { 0, };
+  size_t round = chip.Size() / 8;
+  size_t remain = chip.Size() % 8;
+  for (size_t i = 0; i < round; i++)
+    ((uint64_t *)chip.Array())[i] ^= ((uint64_t *)hash)[i % 8];
+
+  uint8_t *arr = chip.Array() + round * 8;
+  for (size_t i = 0; i < remain; i++)
+    arr[i] ^= hash[i];
+}
