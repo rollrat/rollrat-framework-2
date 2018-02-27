cc25cd7c15fb6baf472b006044f60fbe091536d8
rollrat <rollrat.software@gmail.com>
2018-02-28 00:04:56 +0900

    [Update] Core include file.

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # rollrat-framework-2
+
 Creating a general purpose cpp library.
+
+## Install
+
+```
+git clone https://github.com/rollrat/rollrat-framework-2
+cd /rollrat-framework-2 && mkdir build
+cmake CMakeLists.txt
+cmake --build .
+```
+
+## Example
+
+### BigInteger
+
+``` c++
+#include <rollrat-framework/BigInteger/BigInteger.h>
+
+void test_BigXXX()
+{
+  // test bigbase to_string function
+  BigInteger factorial(1);
+  for (int i = 1; i < 100; i++)
+  {
+    factorial *= i;
+    if (i == 15)
+    cout << i << "!=" << factorial << "\n";
+  }
+
+  // test bigfraction fraction_point
+  BigFraction bf(0);
+  for (int i = 1; i < 100; i++)
+    bf += BigFraction(1, i);
+  cout << bf.fraction_point(10) << "\n";
+  
+  // test get phi approach using bif fraction
+  bf = BigFraction(0);
+  for (int i = 1; i < 500; i++)
+  {
+    bf += BigFraction(1, i * 4 - 3);
+    bf -= BigFraction(1, i * 4 - 1);
+  }
+  cout << (bf * 4).fraction_point(100) << "\n";
+
+  // test init_hex string
+  cout << "x3F4C54109C045923"_i << "\n";
+}
+```
---
+++ include/rollrat-framework.h
@@ -0,0 +1,34 @@
+//===----------------------------------------------------------------------===//
+//
+//                          RollRat-FrameWork V2
+//
+//===----------------------------------------------------------------------===//
+//
+//  rollrat-framework.h - 02/28/2018
+//
+//  Copyright (C) 2018. rollrat. All Rights Reserved.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _ROLLRATFRAMEWORK_9bf1541fdf7efd41b7b39543fd870ac4_
+#define _ROLLRATFRAMEWORK_9bf1541fdf7efd41b7b39543fd870ac4_
+
+#include "rollrat-framework/Algorithm.h"
+#include "rollrat-framework/ChronoTimer.h"
+#include "rollrat-framework/DateTime.h"
+#include "rollrat-framework/File.h"
+#include "rollrat-framework/Random.h"
+#include "rollrat-framework/Sorting.h"
+#include "rollrat-framework/WString.h"
+#include "rollrat-framework/WStringBuffer.h"
+#include "rollrat-framework/WStringBuilder.h"
+
+#include "rollrat-framework/BigInteger/BigFraction.h"
+#include "rollrat-framework/Collections/BinaryTree.h"
+#include "rollrat-framework/Crypto/Crypto.h"
+#include "rollrat-framework/Encoding/Base64Encoding.h"
+#include "rollrat-framework/Encoding/UrlEncoding.h"
+#include "rollrat-framework/Math/Polynomial.h"
+
+
+#endif
